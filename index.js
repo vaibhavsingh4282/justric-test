d9ca4875ff46ccef43eb73342e4c6045be7d807d
Vaibhav Singh <vaibhavsingh4282@gmail.com>
2025-12-25 19:56:19 +0530

    v6

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ import html from './index.html';
 
 export default {
   async fetch(request, env, ctx) {
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    });
+    const url = new URL(request.url);
+
+    // 1. Check if the request is for a static asset (CSS, Images, etc.)
+    if (env.ASSETS) {
+      const response = await env.ASSETS.fetch(request);
+      // If the file is found, return it (this fixes the broken CSS/Images)
+      if (response.status !== 404) {
+        return response;
+      }
+    }
+
+    // 2. Fallback: If no specific file is found, serve the main HTML
+    // Make sure 'index.html' is in your root folder
+    const html = await env.ASSETS.fetch(new Request(new URL('/index.html', url)));
+    return html;
   },
 };
